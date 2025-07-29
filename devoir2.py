7ac0ed6f8dbc0ee94c21b2c00551e2f735e86016
Mahadi106 <mahamatkhatami7@gmail.com>
2025-07-29 23:21:59 +0000

    Add files via upload

---
+++ devoir2.py
@@ -0,0 +1,22 @@
+# Creation d'une liste vide
+my_list = []
+
+my_list.extend([10, 20, 30, 40])
+my_list.insert(1,15)
+ 
+# Etendre la liste 
+my_list.extend([50, 60, 70])
+ 
+# Suppression du dernier element 
+my_list.remove(70)
+
+# Trions la liste par ordre croissant
+my_list.sort()
+print(my_list)
+
+# 7. Rechercher et afficher l'index de la valeur 30
+index_30 = my_list.index(30)
+
+# Affichage des résultats
+print("Liste finale :", my_list)
+print("Index de la valeur 30 :", index_30)
