c0c545f9d6582a4b6c4a0aeadab41147ddae1930
Mahadi106 <mahamatkhatami7@gmail.com>
2025-08-09 21:08:00 +0000

    Add files via upload

---
+++ moyenne1.py
@@ -0,0 +1,10 @@
+# une fonction qui permet de calculer la moyenne 
+note1 = int(input("Saisir la première note"))
+note2 = int(input("Saisir la seconde note"))
+note3 = int(input("Saisir la troisième note"))
+
+# Calcul du resultat
+resultat = (note1 + note2 + note3) / 3
+
+# Affichage du resultat
+print("la moyenne est " ,resultat)
